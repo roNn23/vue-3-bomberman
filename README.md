b22e802336e843d003427e2d767a36a590c1616c
Ronny Neefe <ronny.neefe@aoe.com>
2020-01-12 16:27:23 +0100

    first setup and hello world 🥳💣

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # vue-3-bomberman
 Let's build bomberman in Vue 3 👱‍♂️💣
+
+# Run it
+1. Install: `yarn`
+2. Start it: `yarn start`
+3. Open: http://localhost:8080/
+
+# Useful resources
+- Video: [Hello Vue 3: A First Look at Vue 3 & the Composition API](https://www.youtube.com/watch?v=UAgO2JanN9Y)
+- Setup development environment [vue-next-webpack-preview](https://github.com/vuejs/vue-next-webpack-preview)
+- Blogpost: [Vue 3: Start Using it Today](https://www.vuemastery.com/blog/vue-3-start-using-it-today/)
---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <button @click="showHelloWorld">
+    Click for Hello World!
+  </button>
+  <div v-if="state.clicked">
+    <h1>Hello World 💣</h1>
+  </div>
+</template>
+
+<script>
+import { reactive, computed } from 'vue';
+export default {
+  setup() {
+    const state = reactive({
+      clicked: false
+    });
+
+    const showHelloWorld = () => {
+      state.clicked = true;
+    };
+
+    return {
+      state,
+      showHelloWorld
+    };
+  }
+};
+</script>
+
+<style scoped>
+h1 {
+  font-family: Arial, Helvetica, sans-serif;
+}
+</style>
